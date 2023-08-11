ae4f26e0e9c1d192623da67fae9ef75ff0aa2603
Marco Scheuerer <MarcoScheuerer1978@gmail.com>
2023-08-11 07:36:26 +0200

    Using Lombok @Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString.

--- src/main/java/mao/linatrix/springboot/entity/Product.java
+++ src/main/java/mao/linatrix/springboot/entity/Product.java
@@ -1,6 +1,7 @@
 package mao.linatrix.springboot.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -8,6 +9,11 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Table(
         name="products",
         schema = "ecommerce",
@@ -21,7 +27,6 @@ import java.time.LocalDateTime;
 public class Product {
 
     // define fields
-    // Hibernate creates PRIMARY KEY
     @Id
     @GeneratedValue(strategy=GenerationType.SEQUENCE,
                     generator = "product_generator")
@@ -54,111 +59,5 @@ public class Product {
 
     @UpdateTimestamp
     private LocalDateTime lastUpdated;
-
-    // define constructors
-    public Product() {
-    }
-
-    public Product(Long id, String stockKeepingUnit, String name, String description, BigDecimal price,
-                   boolean active, String imageUrl, LocalDateTime dateCreated, LocalDateTime lastUpdated) {
-        this.id = id;
-        this.sku = stockKeepingUnit;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.active = active;
-        this.imageUrl = imageUrl;
-        this.dateCreated = dateCreated;
-        this.lastUpdated = lastUpdated;
-    }
-
-    // define getter/setter-methods
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getSku() {
-        return sku;
-    }
-
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Boolean getActive() {
-        return active;
-    }
-
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public LocalDateTime getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(LocalDateTime dateCreated) {
-        this.dateCreated = dateCreated;
-    }
-
-    public LocalDateTime getLastUpdated() {
-        return lastUpdated;
-    }
-
-    public void setLastUpdated(LocalDateTime lastUpdated) {
-        this.lastUpdated = lastUpdated;
-    }
-
-    // define toString() method
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", stockKeepingUnit='" + sku + '\'' +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", active=" + active +
-                ", imageUrl='" + imageUrl + '\'' +
-                ", dateCreated=" + dateCreated +
-                ", lastUpdated=" + lastUpdated +
-                '}';
-    }
 
 }
